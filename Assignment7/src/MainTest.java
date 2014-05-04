0b3eebd3454b04b9e7c02934e1cf4b816b7e8101
Admir <Nurkovic>
2014-05-04 16:31:38 +0300

    Subtraction is Wrong

--- Assignment7/src/MainTest.java
+++ Assignment7/src/MainTest.java
@@ -14,5 +14,11 @@ public class MainTest {
 		assertEquals("10 over 5 must be 2",2,tested.divide(10, 5));
 	}
 	
+	/**Writer: Admir Nurkovic*/
+	@Test
+	public void testSubtraction(){
+		assertEquals("20 - 15 must be 5",5,tested.divide(10, 5));
+	}
+	
 
 }
