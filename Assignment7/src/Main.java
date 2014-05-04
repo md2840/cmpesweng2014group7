49108f0d6fd20566d34e12425737d702e0b059d9
sentirelpoder <setenayronael@gmail.com>
2014-05-04 15:41:39 +0300

    Sum method added

--- Assignment7/src/Main.java
+++ Assignment7/src/Main.java
@@ -1,9 +1,22 @@
+import java.util.Random;
+
 
 public class Main {
 	
 	public static void main(String args[]) {
 		System.out.println("Hello world millet");
-		System.out.println("Gokce turkce karakterler gitmiyor");
+		System.out.println("G�k�e turkce karakterler gitmiyor");
 		System.out.println("Merhabaaa admir defatiosikurac ");
+		
+		Random rand = new Random();
+		
+		int choice = rand.nextInt(8);
+		
+		
+	}
+	
+	/**Writer: Setenay Ronael */
+	public static int sum(int a, int b){
+		return a+b+1;
 	}
 }
