9d29abda0734c66c1644156680eed69a987f1da3
Cafer Tayyar <sitivay@gmail.com>
2014-05-04 15:36:15 +0300

    deneme123

--- Assignment7/src/Main.java
+++ Assignment7/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 	
 	public static void main(String args[]) {
 		System.out.println("Hello world millet");
-		System.out.println("G�k�e turkce karakterler gitmiyor");
+		System.out.println("Gokce turkce karakterler gitmiyor");
 		System.out.println("Merhabaaa admir defatiosikurac ");
 	}
 }
