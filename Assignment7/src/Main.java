d60f208e922d2d3ae8b3e3636e2b0ee863ff7c12
Gokce Yesiltas <g.yesiltas@gmail.com>
2014-05-04 15:15:22 +0300

    kj

--- Assignment7/src/Main.java
+++ Assignment7/src/Main.java
@@ -4,5 +4,6 @@ public class Main {
 	public static void main(String args[]) {
 		System.out.println("Hello world millet");
 		System.out.println("G�k�e turkce karakterler gitmiyor");
+		System.out.println("Merhabaaa ");
 	}
 }
