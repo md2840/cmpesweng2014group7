97876c849072e3777fd7751ea7e3b3824867c271
TyCooN <TyCooN@TyCooN-VAIO>
2014-05-04 15:19:23 +0300

    Naper

--- Assignment7/src/Main.java
+++ Assignment7/src/Main.java
@@ -4,6 +4,6 @@ public class Main {
 	public static void main(String args[]) {
 		System.out.println("Hello world millet");
 		System.out.println("G�k�e turkce karakterler gitmiyor");
-		System.out.println("Merhabaaa ");
+		System.out.println("Merhabaaa xXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX ");
 	}
 }
