23733b5762cdd147a100a52c6fa1f9d1bbddb92e
Cafer Tayyar <sitivay@gmail.com>
2014-05-23 17:29:15 +0300

    Comments added

--- Assignment7/src/Main.java
+++ Assignment7/src/Main.java
@@ -1,43 +1,85 @@
 import java.util.Random;
 
+/**
+ *	Main class
+ */
 
 public class Main {
 	
 	public static void main(String args[]) {
 		
 		Random rand = new Random();
+		
+		int choice = rand.nextInt(8);
+		
+		
 	}
 	
-	/**Writer: Setenay Ronael */
+	/**
+	* @author Setenay Ronael
+	* @param a the first argument.
+	* @param b the second argument.
+	* @return sum of a and b
+	*/
 	public static int sum(int a, int b){
 		return a+b;
 	}
 	
-	/**Writer: Gokce Yesiltas */
+	/**
+	* @author Gokce Yesiltas
+	* @param a the first argument.
+	* @param b the second argument.
+	* @return a divided by b
+	*/
 	public static int divide(int a, int b){
 		return a/b;
 	}
 	
-	/**Writer: Cafer Tayyar Yoruk */
+	/**
+	* @author Cafer Tayyar Yoruk
+	* @param a the first argument.
+	* @param b the second argument.
+	* @return multiplication of a and b
+	*/
 	public static int multiply(int a, int b){
 		return a * b;
 	}
 	
-	/**Writer: Mehmet Yasin AKPINAR */
+	/**
+	* @author Mehmet Yasin AKPINAR
+	* @param a the first argument.
+	* @param b the second argument.
+	* @return subtraction of a and b
+	*/
 	public static int subtract(int a, int b){
 		return a - b;
 	}
-	/**Writer: Mustafa Demirel */
+	/**
+	* @author Mustafa Demirel
+	* @param a the first argument.
+	* @param b the second argument.
+	* @return b power of a
+	*/
 	public static double power(int a, int b){
 		return Math.pow(a, b);
 	}
 
-	/**Writer: Admir Nurkovic */
+	/**
+	* @author Admir Nurkovic
+	* @param a the first argument.
+	* @param b the second argument.
+	* @return remained value when a is divided by b
+	*/
 	public static int remainder(int a, int b){
 		return a%b;
 	}
 	
-	/**Writer: Fatih Cataltepe */
+	/**
+	* @author: Fatih Cataltepe 
+	* @param a the first argument.
+	* @param b the second argument.
+	* @return logarithm of a
+	*/
 	public static double logarithm(double a){
 		return Math.log(a);
 	}
--- Assignment7/src/MainTest.java
+++ Assignment7/src/MainTest.java
@@ -1,50 +1,80 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-
+/**
+ * Test class which tests whether methods return expected correct value or not.
+*/
 public class MainTest {
 
 	Main tested = new Main();
 	
 	
-	/**Writer: Setenay Ronael*/
+	/**
+	* @author Setenay Ronael
+	* @see divide( a, b )
+	* @return expected value and value which is returned by divide method
+	*/
 	@Test
 	public void testDivision(){
 		assertEquals("10 over 5 must be 2",2,tested.divide(10, 5));
 	}
 	
-	/**Writer: Admir Nurkovic*/
+	/**
+	* @author Admir Nurkovic
+	* @see subtract( a, b )
+	* @return expected value and value which is returned by subtract method
+	*/
 	@Test
 	public void testSubtraction(){
 		assertEquals("20 - 15 must be 5",5,tested.subtract(20, 15));
 	}
 	
-	/**Writer: Mehmet Yasin AKPINAR*/
+	/**
+	* @author Mehmet Yasin AKPINAR
+	* @see sum( a, b )
+	* @return expected value and value which is returned by sum method
+	*/
 	@Test
 	public void testSummation(){
 		assertEquals("10 plus 5 must be 15",15,tested.sum(10, 5));
 	}
 	
-	/**Writer: Mustafa Demirel*/
+	/**
+	* @author Mustafa Demirel
+	* @see multiply( a, b )
+	* @return expected value and value which is returned by multiply method
+	*/
 	@Test
 	public void testMultiply(){
 		assertEquals("5 times 2 must be 10",10,tested.multiply(5, 2));
 	}
 	
-	/**Writer: Gokce Yesiltas*/
+	/**
+	* @author Gokce Yesiltas
+	* @see power( a, b )
+	* @return expected value and value which is returned by power method
+	*/
 	@Test
 	public void testPower(){
 		assertEquals("2 to 5 must be 32",32,tested.power(2, 5));
 	}
 	
-	/**Writer: Cafer Tayyar Yoruk*/
+	/**
+	* @author Cafer Tayyar Yoruk
+	* @see remainder( a, b )
+	* @return expected value and value which is returned by remainder method
+	*/
 	@Test
 	public void testRemainder(){
 		assertEquals("3 over 2 must be 1",1,tested.remainder(3, 2));
 	}
 	
-	/**Writer: Mehmet Yasin AKPINAR*/
+	/**
+	* @author Mehmet Yasin AKPINAR
+	* @see logarithm( a )
+	* @return expected value and value which is returned by logarithm method
+	*/
 	@Test
 	public void testLogarithm(){
 		assertEquals("Natural logarithm of 1 must be 0",0.0,tested.logarithm(1),0.01);
