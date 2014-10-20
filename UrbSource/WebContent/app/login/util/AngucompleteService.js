05737e245097ad0b769b1d51de5950af27f0dcf7
sentirelpoder <setenayronael@gmail.com>
2014-10-20 21:03:33 +0300

    Login sayfası sorunu spring-security ile halledildi

---
+++ UrbSource/WebContent/app/login/util/AngucompleteService.js
@@ -0,0 +1,26 @@
+services.factory('AngucompleteServiceLogin',['$http',function($http){
+	return {
+		call: function(url,scope,str){
+
+		/**
+		 * Caseler içindeki success methodlarının çağrılma şekilleri angucomplete.js'in çalışması için gereklidir
+		 */
+			switch(url){
+			case 'searchPanel_searchName':
+				console.log("anguService");
+				$http.post('Index/searchName',{params: {
+					query: str
+				}}).
+				success(function(responseData, status, headers, config) {
+					scope.searching = false;
+					scope.processResults(responseData.result);
+				}).
+				error(function(data, status, headers, config) {
+					console.log("error");
+				});
+				break;
+			}
+		}
+	};
+}
+]);
