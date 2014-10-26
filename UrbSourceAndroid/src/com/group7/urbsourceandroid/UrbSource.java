9c9cd87993e9850d889ad58181ec4f3180da4aa0
Dilara Kekulluoglu <dilarahae@gmail.com>
2014-10-26 21:56:52 +0200

    initial android project

---
+++ UrbSourceAndroid/src/com/group7/urbsourceandroid/UrbSource.java
@@ -0,0 +1,20 @@
+package com.group7.urbsourceandroid;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class UrbSource extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
