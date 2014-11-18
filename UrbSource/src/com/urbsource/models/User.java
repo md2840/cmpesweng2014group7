672de4206366bf3d53f64a918ef57cb9506ae394
Dilara Kekulluoglu <dilarahae@gmail.com>
2014-11-18 23:36:32 +0200

    The methods for implementing pbkdf2encryption

--- UrbSource/src/com/urbsource/models/User.java
+++ UrbSource/src/com/urbsource/models/User.java
@@ -1,8 +1,14 @@
 package com.urbsource.models;
 
 import java.io.Serializable;
-import java.util.regex.*;
-import java.math.*;
+import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.util.regex.Pattern;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 
 public class User implements Serializable {
 	
@@ -43,7 +49,9 @@ public class User implements Serializable {
 	}
 	
 	public void setPassword(String password) {
+	
 		this.password = password;
+		
 	}
 
 	public String getFirstName() {
@@ -128,4 +136,42 @@ public class User implements Serializable {
 	public void setNumberOfExperiences(int numberOfExperiences) {
 		this.numberOfExperiences = numberOfExperiences;
 	}
+	
+	/**
+	 * The algorithm for securing passwords
+	 * 
+	 * @author dilara kekulluoglu
+	 */ 
+	private static String generateStrongPasswordHash(String password) throws NoSuchAlgorithmException, InvalidKeySpecException
+    {
+        int iterations = 1000;
+        char[] chars = password.toCharArray();
+        byte[] salt = getSalt().getBytes();
+         
+        PBEKeySpec spec = new PBEKeySpec(chars, salt, iterations, 64 * 8);
+        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        byte[] hash = skf.generateSecret(spec).getEncoded();
+        return iterations + ":" + toHex(salt) + ":" + toHex(hash);
+    }
+     
+    private static String getSalt() throws NoSuchAlgorithmException
+    {
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+        byte[] salt = new byte[16];
+        sr.nextBytes(salt);
+        return salt.toString();
+    }
+     
+    private static String toHex(byte[] array) throws NoSuchAlgorithmException
+    {
+        BigInteger bi = new BigInteger(1, array);
+        String hex = bi.toString(16);
+        int paddingLength = (array.length * 2) - hex.length();
+        if(paddingLength > 0)
+        {
+            return String.format("%0"  +paddingLength + "d", 0) + hex;
+        }else{
+            return hex;
+        }
+    }
 }
