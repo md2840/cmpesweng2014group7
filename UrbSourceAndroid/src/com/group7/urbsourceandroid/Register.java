69a1c0f902c4d3812999479649adc4cf77364daf
Dilara Kekulluoglu <dilarahae@gmail.com>
2014-12-09 17:11:54 +0200

    Android HTTP Post trial

--- UrbSourceAndroid/src/com/group7/urbsourceandroid/Register.java
+++ UrbSourceAndroid/src/com/group7/urbsourceandroid/Register.java
@@ -1,12 +1,30 @@
 package com.group7.urbsourceandroid;
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.ObjectWriter;
+import org.json.JSONException;
+
 import android.app.Activity;
-import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.group7.urbsourceandroid.models.User;
 
 /**
  * 
@@ -16,7 +34,8 @@ import android.widget.TextView;
  */
 
 public class Register extends Activity {
-
+	
+	private Button register;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -25,7 +44,7 @@ public class Register extends Activity {
 
 
 		TextView loginScreen = (TextView) findViewById(R.id.link_to_login);
-
+		
 		// Listening to Login Screen link
 		loginScreen.setOnClickListener(new View.OnClickListener() {
 
@@ -35,14 +54,23 @@ public class Register extends Activity {
 				finish();
 			}
 		});
+		
+		register = (Button) findViewById(R.id.btnRegister);
+		register.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View arg0) {
+				//register(arg0);
+				deneme();
+			}
+		});
 	}
 	/**
 	 * Is called when the register button is clicked on the register page. 
 	 */
 	public void register(View view){
 		String username = ((EditText)findViewById(R.id.reg_username)).getText().toString();
-		String firstname = ((EditText)findViewById(R.id.reg_firstname)).getText().toString();
-		String lastname = ((EditText)findViewById(R.id.reg_lastname)).getText().toString();
+		String firstName = ((EditText)findViewById(R.id.reg_firstname)).getText().toString();
+		String lastName = ((EditText)findViewById(R.id.reg_lastname)).getText().toString();
 		EditText emailT = (EditText) findViewById(R.id.reg_email);
 		String email = emailT.getText().toString();
 		EditText passwordT = (EditText)findViewById(R.id.reg_password);
@@ -60,7 +88,40 @@ public class Register extends Activity {
 			passwordT.setError("Passwords aren't matched.");
 		
 		///////send'em all to API
+//		 HttpClient httpclient = new DefaultHttpClient();
+//		 HttpPost httppost = new HttpPost("http://titan.cmpe.boun.edu.tr:8086/UrbSource/signup/confirm");
+//		 try {
+//		        // Add your data
+//		        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+//		        nameValuePairs.add(new BasicNameValuePair("username", username));
+//		        nameValuePairs.add(new BasicNameValuePair("firstName", firstName));
+//		        nameValuePairs.add(new BasicNameValuePair("lastName", lastName));
+//		        nameValuePairs.add(new BasicNameValuePair("email", email));
+//		        nameValuePairs.add(new BasicNameValuePair("password", password));
+//		        
+//		        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+//
+//		        // Execute HTTP Post Request
+//		        HttpResponse response = httpclient.execute(httppost);
+//		        Log.i("geldi mi",response.toString());
+//
+//		    } catch (ClientProtocolException e) {
+//		        // TODO Auto-generated catch block
+//		    } catch (IOException e) {
+//		        // TODO Auto-generated catch block
+//		    }
 
+		new MyAsyncTask().execute(username,firstName,lastName,email,password,password2);	
+	}
+	public void deneme(){
+		
+		String username="dilara91";
+		String firstName = "Dilara";
+		String lastName = "Kek";
+		String email = "dilara.kekulluoglu@boun.edu.tr";
+		String password = "123456";
+		String password2 = "123456";
+		new MyAsyncTask().execute(username,firstName,lastName,email,password,password2);	
 	}
 	public final static boolean isValidEmail(CharSequence target) {
 		if (TextUtils.isEmpty(target)) {
@@ -68,6 +129,83 @@ public class Register extends Activity {
 		} else {
 			return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
 		}
+	}
+	
+	private class MyAsyncTask extends AsyncTask<String, Integer, Double>{
+		 
+		@Override
+		protected Double doInBackground(String... params) {
+			// TODO Auto-generated method stub
+			try {
+				postData(params[0],params[1],params[2],params[3],params[4],params[5]);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;
+		}
+ 
+		protected void onPostExecute(Double result){
+			Toast.makeText(getApplicationContext(), "command sent", Toast.LENGTH_LONG).show();
+		}
+		
+ 
+		public void postData(String username, String firstName,String lastName,String email,String password,String password2) throws JSONException {
+			// Create a new HttpClient and Post Header
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpPost httppost = new HttpPost("http://titan.cmpe.boun.edu.tr:8086/UrbSource/signup/confirm");
+ 
+			try {
+				// Add your data
+//				JSONObject jsonobj = new JSONObject();
+//				jsonobj.put("username",username);
+//				jsonobj.put("firstName",firstName);
+//				jsonobj.put("lastName",lastName);
+//				jsonobj.put("email",email);
+//				jsonobj.put("password",password);
+//				StringEntity se = new StringEntity(jsonobj.toString());    
+//				se.setContentType("application/json;charset=UTF-8");
+//				se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE,"application/json;charset=UTF-8"));
+//				httppost.setHeader("Content-Type", "application/json");
+//				 httppost.setHeader("Accept", "application/json");
+//			
+//				httppost.setEntity(se);
+//				
+//				// Execute HTTP Post Request
+//				HttpResponse response = httpclient.execute(httppost);
+				
+				User u= new User();
+				u.setUsername(username);
+				u.setFirstName(firstName);
+				u.setLastName(lastName);
+				u.setEmail(email);
+				u.setPassword(password);
+				u.setPassword2(password2);
+				Log.i("parametreler",username);
+				ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+				String json = ow.writeValueAsString(u);
+				 Log.i("json",json);
+				 
+				 //httppost.setHeader("Content-Type", "application/json");
+				  StringEntity se = new StringEntity(json);    
+				 se.setContentType("application/json;charset=UTF-8");
+				 se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE,"application/json;charset=UTF-8"));
+
+				 httppost.setEntity(se);
+					
+					// Execute HTTP Post Request
+					HttpResponse response = httpclient.execute(httppost);
+					
+				 Log.i("geldi mi",response.getStatusLine().toString());
+
+ 
+			} catch (ClientProtocolException e) {
+				// TODO Auto-generated catch block
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+			}
+		}
+ 
 	}
 
 }
---
+++ UrbSourceAndroid/src/com/group7/urbsourceandroid/models/User.java
@@ -0,0 +1,136 @@
+package models;
+
+import java.io.Serializable;
+import java.util.regex.Pattern;
+
+
+
+public class User implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String email;
+
+	private int expPoints=0;
+	private int commentPoints=0;
+	/**
+	 * For password confirmation
+	 */
+	private String password2;
+	private int numberOfExperiences;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+	
+		this.password = password;
+		
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword2() {
+		return password2;
+	}
+
+	public void setPassword2(String password2) {
+		this.password2 = password2;
+	}
+
+	public boolean isEmailValid() {
+		return Pattern.matches("\\w+@\\w+(\\w|[.-_])*\\.[a-zA-Z]+", email);
+	}
+
+	/**
+	 * Karma is kind of user level that is dependent on both experience and
+	 * comment points. We currently define it with formula :
+	 * 
+	 * floor(log(karma = alpha * expPoints + beta * commentPoints + 1)) + 1
+	 * 
+	 * where alpha and beta are coefficients and value in log is >= 1 so that
+	 * karma is positive.
+	 * 
+	 * @return karma of the user.
+	 */
+	public int getKarma() {
+		double pureKarma = expPoints + commentPoints + 1;
+		if (pureKarma < 1)
+			pureKarma = 1;
+		return (int)Math.log(pureKarma) + 1;
+	}
+
+	public int getExperiencePoints() {
+		return expPoints;
+	}
+
+	public void setExperiencePoints(int expPoints) {
+		this.expPoints = expPoints;
+	}
+
+	public int getCommentPoints() {
+		return commentPoints;
+	}
+
+	public void setCommentPoints(int commentPoints) {
+		this.commentPoints = commentPoints;
+	}
+	
+	public int getNumberOfExperiences() {
+		return numberOfExperiences;
+	}
+	
+	/**
+	 * To be only used from DAO
+	 * 
+	 * @param numberOfExperiences
+	 */
+	public void setNumberOfExperiences(int numberOfExperiences) {
+		this.numberOfExperiences = numberOfExperiences;
+	}
+	
+	
+}
