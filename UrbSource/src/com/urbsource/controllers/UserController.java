23b143d6e30cc4a9017ed51d1d8f7e7361e39002
Gokce Yesiltas <g.yesiltas@gmail.com>
2014-12-30 17:40:53 +0200

    User profile web api and page for mobile application.

--- UrbSource/src/com/urbsource/controllers/UserController.java
+++ UrbSource/src/com/urbsource/controllers/UserController.java
@@ -3,6 +3,17 @@
  */
 package com.urbsource.controllers;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -14,6 +25,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.urbsource.db.JDBCUserDAO;
@@ -126,7 +138,7 @@ public class UserController {
 		}
 		return new ModelAndView("forgot_pw", "command", u);
 	}
-	
+
 	/***
 	 * Handles GET requests to forgot password page and renders forgot password form.
 	 * 
@@ -140,4 +152,65 @@ public class UserController {
 		return new ModelAndView("forgot_pw");
 	}
 
+	/**
+	 * Handles HTTP get requests for user information page from mobile application.
+	 * 
+	 * 
+	 * @author Gokce Yesiltas 
+	 * @param request
+	 * @param response
+	 * @return
+	 * @throws JSONException
+	 * @throws IOException
+	 */
+	@RequestMapping(value="/mobileuserinfo", method=RequestMethod.POST)
+	public @ResponseBody HashMap<String,Object> userInfo(HttpServletRequest request, HttpServletResponse response) throws JSONException, IOException{
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		JSONObject json = (new JSONObject(getBody(request))).getJSONObject("result");
+		String username;
+		try {
+			username = json.getJSONObject("params").getString("username");
+			User u = userDao.getLoginUser(username);
+			map.put("expPoint", u.getExperiencePoints());
+			map.put("commentPoint", u.getCommentPoints());
+			map.put("numOfExp", u.getNumberOfExperiences());
+
+		} catch (JSONException e) {
+			throw e;
+		}
+		return map;
+	}
+
+	public String getBody(HttpServletRequest request) throws IOException
+	{
+		String body = null;
+		StringBuilder stringBuilder = new StringBuilder();
+		BufferedReader bufferedReader = null;
+		try {
+			InputStream inputStream = request.getInputStream();
+			if (inputStream != null) {
+				bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+				char[] charBuffer = new char[128];
+				int bytesRead = -1;
+				while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+					stringBuilder.append(charBuffer, 0, bytesRead);
+				}
+			} else {
+				stringBuilder.append("");
+			}
+		} catch (IOException ex) {
+			throw ex;
+		} finally {
+			if (bufferedReader != null) {
+				try {
+					bufferedReader.close();
+				} catch (IOException ex) {
+					throw ex;
+				}
+			}
+		}
+		body = stringBuilder.toString();
+		body = "{ \"result\": "+body + "}";
+		return body;
+	}
 }
--- UrbSourceAndroid/src/com/group7/urbsourceandroid/UserProfile.java
+++ UrbSourceAndroid/src/com/group7/urbsourceandroid/UserProfile.java
@@ -1,29 +1,111 @@
 package com.group7.urbsourceandroid;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.group7.urbsourceandroid.models.User;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class UserProfile extends Activity {
+	private SessionManager session;
+	private String username;
+	private User user;
+	private String responseText;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Log.i("started", "started");
 		setContentView(R.layout.userprofile);
-		
-		//Username of user is updated in the page
-		String usernameS = "gokce";
-		Log.i("user", usernameS);
+
+		session = new SessionManager(getApplicationContext());
+		session.checkLogin();
+		username = session.getUserDetails().get("name");
+
+		try {
+			getUserData();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		TextView username = (TextView) findViewById(R.id.pro_username);
-		username.setText(usernameS);
-		
-		//Point of user is updated in the page
-		int pointI = 13;
-		String pointS = Integer.toString(pointI);
+		username.setText(this.username);
+
+		String expPointS = Integer.toString(user.getExperiencePoints());
 		TextView point = (TextView) findViewById(R.id.pro_point);
-		point.setText(pointS);
-		
+		point.setText(expPointS);
+
+	}
+
+
+	public void getUserData() throws JSONException {
+		// Create a new HttpClient and Post Header
+		HttpClient httpclient = new DefaultHttpClient();
+		//HttpGet httpGet = new HttpGet("http://titan.cmpe.boun.edu.tr:8086/UrbSource/experience/recent");
+		HttpPost httpPost = new HttpPost("http://10.0.3.2/UrbSource/user/mobileuserinfo"); //for genymotion
+
+		try {
+
+			// Add your data
+			JSONObject jsonobj = new JSONObject();
+			jsonobj.put("username", username);
+
+			StringEntity se = new StringEntity(jsonobj.toString());    
+			se.setContentType("application/json;charset=UTF-8");
+			se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE,"application/json;charset=UTF-8"));
+			httpPost.setHeader("Content-Type", "application/json");
+			httpPost.setHeader("Accept", "application/json");
+
+			httpPost.setEntity(se);
+			HttpResponse response = httpclient.execute(httpPost);
+			HttpEntity entity = response.getEntity(); 
+			responseText = getASCIIContentFromEntity(entity);
+			
+			JSONObject obj = new JSONObject(responseText);
+			user.setExperiencePoints(obj.getInt("expPoint"));
+			user.setCommentPoints(obj.getInt("commentPoint"));
+			user.setNumberOfExperiences(obj.getInt("numOfExp"));
+
+
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+		}
+	}
+
+	public String getASCIIContentFromEntity(HttpEntity entity) throws IllegalStateException, IOException {
+		InputStream in = entity.getContent();
+
+
+		StringBuffer out = new StringBuffer();
+		int n = 1;
+		while (n>0) {
+			byte[] b = new byte[4096];
+			n =  in.read(b);
+
+
+			if (n>0) out.append(new String(b, 0, n));
+		}
+
+
+		return out.toString();
 	}
 }
