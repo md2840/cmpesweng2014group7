741fbc3d29ad9c52763f7364f38f773e734a3496
Gokce Yesiltas <g.yesiltas@gmail.com>
2015-01-07 00:35:06 +0200

    Search button was added to settings fragment

--- UrbSourceAndroid/src/com/group7/urbsourceandroid/SettingsFragment.java
+++ UrbSourceAndroid/src/com/group7/urbsourceandroid/SettingsFragment.java
@@ -10,37 +10,46 @@ import android.widget.Button;
 
 public class SettingsFragment extends Fragment {
 
-	private Button UserProfile, Logout;
-	
+	private Button UserProfile, Logout, SearchExperience;
+
 	private SessionManager session;
-	
+
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
- 
-        View view = inflater.inflate(R.layout.settings, container, false);
-        
-        session = new SessionManager(getActivity().getApplicationContext());
-        session.checkLogin();
-        
-        UserProfile = (Button) view.findViewById(R.id.userprofile);
-        UserProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            	Intent i = new Intent(getActivity().getApplicationContext(), UserProfile.class);
-                startActivity(i);
-            }
-            });
-        
-       Logout = (Button) view.findViewById(R.id.logout);
-       Logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            	session.logoutUser();
-            	
-            }
-            });
-        return view;
+			Bundle savedInstanceState) {
+
+		View view = inflater.inflate(R.layout.settings, container, false);
+
+		session = new SessionManager(getActivity().getApplicationContext());
+		session.checkLogin();
+
+		UserProfile = (Button) view.findViewById(R.id.userprofile);
+		UserProfile.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(getActivity().getApplicationContext(), UserProfile.class);
+				startActivity(i);
+			}
+		});
+
+		SearchExperience = (Button) view.findViewById(R.id.searchpage);
+		SearchExperience.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(getActivity().getApplicationContext(), SearchExperience.class);
+				startActivity(i);
+			}
+		});
+
+		Logout = (Button) view.findViewById(R.id.logout);
+		Logout.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				session.logoutUser();
+
+			}
+		});
+		return view;
 	}
-	
-   
+
+
 }
